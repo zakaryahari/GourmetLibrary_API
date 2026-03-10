9cb445093a286642900b2a66b1d2cf6d7e738bbd
zakarya hari <zakariahari42@gmail.com>
2026-03-10 12:25:35 +0000

    feat: Modèle Book avec relations et attributs

---
+++ app/Models/Book.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Book extends Model
+{
+    /** @use HasFactory<\Database\Factories\BookFactory> */
+    use HasFactory;
+
+    protected $table = 'books';
+    protected $fillable = ['title', 'chef', 'description', 'slug', 'category_id'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function copies(): HasMany
+    {
+        return $this->hasMany(Copy::class);
+    }
+}
