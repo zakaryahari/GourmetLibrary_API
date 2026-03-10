6270299727ffc1f3f5ff77e7462dcc91d086857f
zakarya hari <zakariahari42@gmail.com>
2026-03-10 12:25:31 +0000

    feat: Migration table damage_reports

---
+++ database/migrations/2026_03_09_143019_create_damage_reports_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('damage_reports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('copy_id')->constrained()->onDelete('cascade');
+            $table->text('description');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('damage_reports');
+    }
+};
