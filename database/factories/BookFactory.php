e0547e387b66fced86ee320ad22e32e6dd42901f
zakarya hari <zakariahari42@gmail.com>
2026-03-10 12:25:39 +0000

    feat: Factory pour générer des données de livres

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class BookFactory extends Factory
+{
+    protected $model = Book::class;
+
+    public function definition(): array
+    {
+        $title = $this->faker->sentence(3);
+        
+        return [
+            'title' => $title,
+            'chef' => $this->faker->name(),
+            'description' => $this->faker->paragraph(),
+            'slug' => Str::slug($title),
+            'category_id' => Category::factory(),
+        ];
+    }
+}
