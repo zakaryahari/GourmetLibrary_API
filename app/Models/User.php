b7d5b6361373a8e13d9adef119759602aeda98fd
zakarya hari <zakariahari42@gmail.com>
2026-03-10 12:25:33 +0000

    feat: Modèle User avec relations et attributs

---
+++ app/Models/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    protected $table = 'users';
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    public function copies()
+    {
+        return $this->belongsToMany(Copy::class, 'borrows')
+            ->using(Borrow::class)
+            ->withPivot('borrowed_at', 'returned_at')
+            ->withTimestamps();
+}
