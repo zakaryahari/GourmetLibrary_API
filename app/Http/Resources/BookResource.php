291ffe13228129b3beb3c41279d8e808bb27108d
zakarya hari <zakariahari42@gmail.com>
2026-03-10 12:25:44 +0000

    feat: Resource pour formater les données des livres

---
+++ app/Http/Resources/BookResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'chef' => $this->chef,
+            'slug' => $this->slug,
+            'category' => $this->category->name,
+        ];
+    }
+}
